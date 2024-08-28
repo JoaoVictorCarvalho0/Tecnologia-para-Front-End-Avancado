b8e86f4a8ecf57f0527f5d995522f0c0486ad63e
João Victor <jotavezincarvalho20@icloud.com>
2024-08-28 20:57:56 -0300

    aula02

---
+++ aula02/aula02/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TrabalhandoComImagens from './components/TrabalhandoComImagens'
+import RenderizandoComFuncoes from './components/RenderizandoComFuncoes'
+const App = () => {
+  return (
+    <div>
+      <RenderizandoComFuncoes />
+      <TrabalhandoComImagens/>
+    </div>
+  )
+}
+
+export default App
---
+++ aula02/aula02/src/components/TrabalhandoComImagens.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Flamengo from '../assets/framengo.webp'
+const TrabalhandoComImagens = () => {
+  return (
+    <div>
+      <img src="flacampeaobras.webp" alt="" />
+      <img src= {Flamengo} alt="" />
+    </div>
+  )
+}
+
+export default TrabalhandoComImagens
